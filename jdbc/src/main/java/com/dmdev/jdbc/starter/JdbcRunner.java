afe70181e1cd1bb6782e32c7f19a7c196f58eeee
Shaldryn <Shaldryn@yandex.ru>
2022-06-28 22:57:02 +0300

    add transaction, batch, statement

--- jdbc/src/main/java/com/dmdev/jdbc/starter/JdbcRunner.java
+++ jdbc/src/main/java/com/dmdev/jdbc/starter/JdbcRunner.java
@@ -1,16 +1,135 @@
 package com.dmdev.jdbc.starter;
 
 import com.dmdev.jdbc.starter.util.ConnectionManager;
-import org.postgresql.Driver;
 
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JdbcRunner {
 
     public static void main(String[] args) throws SQLException {
-        Class<Driver> driverClass = Driver.class;
+//        Class<Driver> driverClass = Driver.class;
+////        String sql = "CREATE TABLE IF NOT EXISTS info(" +
+////                "id SERIAL PRIMARY KEY," +
+////                "data TEXT NOT NULL" +
+////                ");";
+////        String sql = "INSERT INTO info (data)" +
+////                "VALUES" +
+////                "('Test1')," +
+////                "('Test2')," +
+////                "('Test3')," +
+////                "('Test4');";
+////        String sql = "UPDATE info SET data = 'TestTest' WHERE id = 4;";
+//        String sql = "SELECT * FROM ticket;";
+//        try (var connection = ConnectionManager.open();
+////             var statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)) { //включаем возможность изменять данные в ResultSet(почти не используется, только для выборки)
+//             var statement = connection.createStatement()) {
+//            System.out.println(connection.getSchema());
+//            System.out.println(connection.getTransactionIsolation());
+////            var executeResult = statement.execute(sql);
+////            var executeResult = statement.executeUpdate(sql);
+////            var executeResult = statement.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+////            var generatedKeys = statement.getGeneratedKeys();
+////            System.out.println(executeResult);
+////            System.out.println(statement.getUpdateCount());
+//            var executeResult = statement.executeQuery(sql);
+//            while (executeResult.next()) {
+//                System.out.println(executeResult.getLong("id"));
+//                System.out.println(executeResult.getString("passenger_no"));
+//                System.out.println(executeResult.getBigDecimal("cost"));
+//                System.out.println("-------");
+//            }
+//        }
+
+//        String flightId = "2 OR 1 = 1; DROP TABLE info;"; //SQL Injection
+//        var result = getTicketsByFlightId(flightId);
+//        System.out.println(result);
+
+//        Long flightId = 2L;
+//        var result = getTicketsByFlightId(flightId);
+//        System.out.println(result);
+
+//        var result = getFlightsBetween(LocalDate.of(2020, 10, 1).atStartOfDay(), LocalDateTime.now());
+//        System.out.println(result);
+
+        checkMetaData();
+    }
+
+//    private static List<Long> getTicketsByFlightId(String flightId) throws SQLException {
+//        String sql = String.format("SELECT id FROM ticket WHERE flight_id = %s", flightId);
+//        List<Long> result = new ArrayList<>();
+//        try (var connection = ConnectionManager.open();
+//             var statement = connection.createStatement()) {
+//            var resultSet = statement.executeQuery(sql);
+//            while (resultSet.next()) {
+////                result.add(resultSet.getLong("id"));
+//                result.add(resultSet.getObject("id", Long.class)); //если поле может иметь значение NULL
+//            }
+//        }
+//        return result;
+//    }
+
+    private static List<Long> getTicketsByFlightId(Long flightId) throws SQLException {
+        String sql = "SELECT id FROM ticket WHERE flight_id = ?";
+        List<Long> result = new ArrayList<>();
+        try (var connection = ConnectionManager.open();
+             var preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, flightId);
+            var resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+//                result.add(resultSet.getLong("id"));
+                result.add(resultSet.getObject("id", Long.class)); //если поле может иметь значение NULL
+            }
+        }
+        return result;
+    }
+
+    private static List<Long> getFlightsBetween(LocalDateTime start, LocalDateTime end) throws SQLException {
+        String sql = "SELECT id FROM flight WHERE departure_date BETWEEN ? AND ?";
+        List<Long> result = new ArrayList<>();
+        try (var connection = ConnectionManager.open();
+             var preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setFetchSize(50);
+            preparedStatement.setQueryTimeout(10);
+            preparedStatement.setMaxRows(100);
+
+            System.out.println(preparedStatement);
+            preparedStatement.setTimestamp(1, Timestamp.valueOf(start));
+            System.out.println(preparedStatement);
+            preparedStatement.setTimestamp(2, Timestamp.valueOf(end));
+            System.out.println(preparedStatement);
+
+            var resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                result.add(resultSet.getObject("id", Long.class)); // не getLong для избегания лишнего boxing/unboxing
+            }
+            return result;
+        }
+    }
+
+    private static void checkMetaData() throws SQLException {
         try (var connection = ConnectionManager.open()) {
-            System.out.println(connection.getTransactionIsolation());
+            var metaData = connection.getMetaData();
+            var catalogs = metaData.getCatalogs();
+            while (catalogs.next()) {
+                var catalog = catalogs.getString(1);
+//                System.out.println(catalog);
+                var schemas = metaData.getSchemas();
+                while (schemas.next()) {
+                    var schema = schemas.getString("TABLE_SCHEM");
+//                    System.out.println(schema);
+//                    var tables = metaData.getTables(catalog, schema, "%", null);
+                    var tables = metaData.getTables(catalog, schema, "%", new String[] {"TABLE"});
+                    if (schema.equals("public")) {
+                        while (tables.next()) {
+                            System.out.println(tables.getString("TABLE_NAME"));
+                        }
+                    }
+                }
+            }
         }
     }
 }
---
+++ jdbc/src/main/java/com/dmdev/jdbc/starter/TransactionRunner.java
@@ -0,0 +1,83 @@
+package com.dmdev.jdbc.starter;
+
+import com.dmdev.jdbc.starter.util.ConnectionManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TransactionRunner {
+
+    public static void main(String[] args) throws SQLException {
+//        transaction();
+        batchQuery();
+    }
+
+    private static void transaction() throws SQLException {
+        var flightId = 9L;
+        var deleteFlightSql = "DELETE FROM flight WHERE id = ?";
+        var deleteTicketsSql = "DELETE FROM ticket WHERE flight_id = ?";
+        Connection connection = null;
+        PreparedStatement deleteFLightStatement = null;
+        PreparedStatement deleteTicketsStatement = null;
+        try {
+            connection = ConnectionManager.open();
+            deleteFLightStatement = connection.prepareStatement(deleteFlightSql);
+            deleteTicketsStatement = connection.prepareStatement(deleteTicketsSql);
+
+            connection.setAutoCommit(false);
+
+            deleteFLightStatement.setLong(1, flightId);
+            deleteTicketsStatement.setLong(1, flightId);
+
+            deleteTicketsStatement.executeUpdate();
+            deleteFLightStatement.executeUpdate();
+            connection.commit();
+        } catch (Exception e) {
+            if (connection != null) {
+                connection.rollback();
+            }
+            throw e;
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+            if (deleteFLightStatement != null) {
+                deleteFLightStatement.close();
+            }
+            if (deleteTicketsStatement != null) {
+                deleteTicketsStatement.close();
+            }
+        }
+    }
+
+    private static void batchQuery() throws SQLException {
+        var flightId = 9L;
+        var deleteFlightSql = "DELETE FROM flight WHERE id = " + flightId;
+        var deleteTicketsSql = "DELETE FROM ticket WHERE flight_id = " + flightId;
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            connection = ConnectionManager.open();
+            connection.setAutoCommit(false);
+
+            statement = connection.createStatement();
+            statement.addBatch(deleteTicketsSql);
+            statement.addBatch(deleteFlightSql);
+
+            var ints = statement.executeBatch();
+
+            connection.commit();
+        } catch (Exception e) {
+            if (connection != null) {
+                connection.rollback();
+            }
+            throw e;
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+}
