435895dd042959dbb944dc7cafdab1647088af07
Shaldryn <Shaldryn@yandex.ru>
2022-08-10 21:52:21 +0300

    add lombok example

--- http-servlets-starter/src/main/java/com/dmdev/http/util/ConnectionManager.java
+++ http-servlets-starter/src/main/java/com/dmdev/http/util/ConnectionManager.java
@@ -1,17 +1,20 @@
 package com.dmdev.http.util;
 
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 /**
  * полная реализация приведена в jdbc
  */
-public final class ConnectionManager {
+@UtilityClass
+public class ConnectionManager {
 
-    public static final String URL_KEY = "db.url";
-    public static final String USER_KEY = "db.user";
-    public static final String PASSWORD_KEY = "db.password";
+    private static final String URL_KEY = "db.url";
+    private static final String USER_KEY = "db.user";
+    private static final String PASSWORD_KEY = "db.password";
 
     static {
         loadDriver();
@@ -25,16 +28,10 @@ public final class ConnectionManager {
         }
     }
 
-    private ConnectionManager() {
-    }
-
+    @SneakyThrows
     public static Connection get() {
-        try {
-            return DriverManager.getConnection(PropertiesUtil.get(URL_KEY),
-                    PropertiesUtil.get(USER_KEY),
-                    PropertiesUtil.get(PASSWORD_KEY));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return DriverManager.getConnection(PropertiesUtil.get(URL_KEY),
+                PropertiesUtil.get(USER_KEY),
+                PropertiesUtil.get(PASSWORD_KEY));
     }
 }
